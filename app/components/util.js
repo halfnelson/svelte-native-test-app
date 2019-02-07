94a150adfa8322508e1bc2e57b07afa433f12e43
halfnelson <dpershouse@gmail.com>
2019-02-07 23:04:12 +1000

    removed components

--- app/components/util.js
+++
@@ -1,9 +0,0 @@
-export function cleanProps(props) {
-    let newProps = {}
-    for(let key of Object.keys(props)) {
-        if (!key.startsWith('$')) {
-            newProps[key] = props[key];
-        }
-    }
-    return newProps;
-}
